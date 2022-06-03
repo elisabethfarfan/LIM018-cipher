5f0399806cdcb19aad00a816e0085434a8aab1a5
elisabethfarfan <elisabethfarfan@gmail.com>
2022-06-03 16:41:13 -0500

    css, cambios en el diseño

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import cipher from './cipher.js';
 
+const btnName = document.getElementById("btnName");
+btnName.addEventListener("click",extraerName);
 
 const btnCifrar = document.getElementById("btnCifrar");
 btnCifrar.addEventListener("click",cifrar);
 
 const btnDescifrar = document.getElementById("btnDescifrar");
 btnDescifrar.addEventListener("click",descifrar);
+
+const btnEnviar = document.getElementById("btnEnviar");
+btnEnviar.addEventListener("click",mensajeFinal);
 
 
 function validar(){
@@ -22,13 +27,23 @@ function validar(){
     }
 
 }
+function extraerName(){
+    document.getElementById("form-container").style.display = 'block';
+    const lblNombre = document.getElementById("name").value;
+   
+    document.getElementById('lbl_name_recib').innerHTML= 'BIENVENID@  '+
+     lblNombre.toUpperCase();
 
+    document.getElementById("form-login").style.display = 'none';
+    
+
+}
 function cifrar(){
     validar();
     const mensajeCifrar = document.getElementById('txtMensajeCifrar').value;
     const offset = document.getElementById('txtOffset').value;
      //cipher.encode(offset,mensajeCifrar)
-    document.getElementById('txtCifrado').value= cipher.encode(offset,mensajeCifrar);
+    document.getElementById('txtMensajeCifrar').value= cipher.encode(offset,mensajeCifrar);
     
 }
 
@@ -37,6 +52,14 @@ function descifrar(){
     const mensajeDescifrar = document.getElementById('txtMensajeCifrar').value;
     const offset = document.getElementById('txtOffset').value;
      //cipher.encode(offset,mensajeCifrar)
-    document.getElementById('txtCifrado').value= cipher.decode(offset,mensajeDescifrar);
+    document.getElementById('txtMensajeCifrar').value= cipher.decode(offset,mensajeDescifrar);
+
+}
+
+function mensajeFinal(){
+    const lblNombre = document.getElementById("nameSend").value;
+    document.getElementById("form-container").style.display = 'none';
+    document.getElementById('mensajeFinal').innerHTML= 'MENSAJE ENVIADO A '+
+     lblNombre.toUpperCase();
 
 }
